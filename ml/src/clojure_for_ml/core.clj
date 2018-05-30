d4424aa9321d2c0e50480f504205299d944a0dce
Arthur <arturaliiev@gmail.com>
2018-05-30 23:58:46 +0300

    some starting code

--- ml/src/clojure_for_ml/core.clj
+++ ml/src/clojure_for_ml/core.clj
@@ -1,13 +1,18 @@
 (ns clojure-for-ml.core
-  (:use [clojure.core.matrix]))
-
-(def ma (matrix [[0 1 2] [3 4 5]]))
-
-(pm ma)
-
-(comment
-  (use '(incanter core stats charts io))
-  (view (histogram (sample-normal 1000))))
+  (:require
+    [clojure.core.matrix :refer [matrix]]
+    [incanter.core :refer [view]]
+    [incanter.charts :refer [histogram scatter-plot]]
+    [incanter.stats :refer [linear-model]]))
 
 
-(def tra)
+(def X (matrix [8.401 14.475 13.396 12.127 5.044
+                    8.339 15.692 17.108 9.253 12.029]))
+(def Y (matrix [-1.57 2.32 0.424 0.814 -2.3
+                    0.01 1.954 2.296 -0.635 0.328]))
+
+(def linear-samp-scatter
+  (scatter-plot X Y))
+
+(defn plot-scatter []
+  (view linear-samp-scatter))
